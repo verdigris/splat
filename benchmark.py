4d61c4ebd6ae9f2e027534b1f063334f07e77508
Guillaume Tucker <guillaume@mangoz.org>
2012-03-25 07:12:02 +0100

    add benchmark + clean-up test

---
+++ benchmark.py
@@ -0,0 +1,29 @@
+import sys
+import time
+sys.path.append('..')
+import geomusic as geo
+
+def bm(name, call, *args, **kw):
+    start = time.time()
+    ret = call(*args, **kw)
+    stop = time.time()
+    time_ms = (stop - start) * 1000
+    print('{0}: {1:.3f}ms'.format(name, time_ms))
+    return ret
+
+def main(argv):
+    time_s = 10.0
+    frag = bm('Fragment', geo.Fragment, 2, 48000)
+    bm('resize', frag.resize, time_s)
+    gen = bm('Generator', geo.Generator, frag)
+    bm('set_levels', gen.set_levels, ((0.7, 0.7)))
+    bm('sine', gen.sine, 1000, 0, time_s)
+    raw = bm('get_raw_bytes', frag.get_raw_bytes, 2)
+    return True
+
+if __name__ == '__main__':
+    ret = main(sys.argv)
+    if ret is True:
+        sys.exit(0)
+    else:
+        sys.exit(1)
--- test.py
+++ test.py
@@ -4,23 +4,7 @@ import math
 sys.path.append('..')
 import geomusic as geo
 
-if len(sys.argv) > 1:
-    mode = sys.argv[1]
-else:
-    mode = 'exp'
-
-if len(sys.argv) > 2:
-    file_name = sys.argv[2]
-else:
-    file_name = 'test.wav'
-
-print("mode: {0}, file name: {1}".format(mode, file_name))
-
-random.seed()
-frag = geo.Fragment(2, 48000)
-gen = geo.Generator(frag)
-gen.set_levels((0.7, 0.7))
-
+# Fixed parameters
 l = 7.0 # total length in seconds
 p = 8.0 # sounds per seconds
 b = 55.0 # base frequency in Hz
@@ -32,17 +16,41 @@ modes = {
     'exp': (math.log(m, 2), lambda b, k, r: b * 2 ** (k * r)),
 }
 
-k, func = modes[mode]
-n = int(l * p)
-t = 0.0
+def main(argv):
+    if len(argv) > 1:
+        mode = argv[1]
+    else:
+        mode = 'exp'
 
-for i in range(n):
-    f = func(b, k, random.random())
-    start = t
-    stop = t + l / n
-    t = stop
-    gen.sine(f, start, stop)
+    if len(argv) > 2:
+        file_name = argv[2]
+    else:
+        file_name = 'test.wav'
 
-frag.save_to_file(file_name, 2)
+    print("mode: {0}, file name: {1}".format(mode, file_name))
 
-sys.exit(0)
+    frag = geo.Fragment(2, 48000)
+    gen = geo.Generator(frag)
+    gen.set_levels((0.7, 0.7))
+    k, func = modes[mode]
+    n = int(l * p)
+    t = 0.0
+    random.seed()
+
+    for i in range(n):
+        f = func(b, k, random.random())
+        start = t
+        stop = t + l / n
+        t = stop
+        gen.sine(f, start, stop)
+
+    frag.save_to_file(file_name, 2)
+
+    return True
+
+if __name__ == '__main__':
+    ret = main(sys.argv)
+    if ret is True:
+        sys.exit(0)
+    else:
+        sys.exit(1)
