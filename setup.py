949d04cb09ce5d945a90e88a47a59a2001053a84
Guillaume Tucker <guillaume@mangoz.org>
2012-05-19 11:59:09 +0100

    setup.py: fix package name

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 
-setup(name='_geomusic', version='0.0',
+setup(name='geomusic', version='0.0',
       ext_modules=[Extension('_geomusic', ['_geomusic.c'])],
       packages=['geomusic'])
