f19dc0816f11c877c5788b7b5b4319a9d55dae54
Guillaume Tucker <guillaume@mangoz.org>
2013-03-12 23:23:43 +0000

    geomusic: add docstrings and move FilterChain to filters
    * move FilterChain class from generator.py to filters.py and
      import it in __init__.py
    * add and improve docstrings in all classes and functions in
      generator.py, filters.py and data.py

--- geomusic/__init__.py
+++ geomusic/__init__.py
@@ -1,6 +1,7 @@
 from _geomusic import lin2dB, dB2lin
 from generator import Generator, FilterChain, SineGenerator, OvertonesGenerator
 from data import Fragment
+from filters import FilterChain
 import filters
 import sources
 import scales
