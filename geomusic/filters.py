5700e01bfcf63caee80468839acc0974cf3a74fc
Guillaume Tucker <guillaume@mangoz.org>
2013-03-15 08:55:29 +0000

    add LGPL v3 license + copyright statements

--- geomusic/filters.py
+++ geomusic/filters.py
@@ -1,3 +1,20 @@
+# Geomusic - geomusic/filters.py
+#
+# Copyright (C) 2012, 2013 Guillaume Tucker <guillaume@mangoz.org>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import collections
 from _geomusic import dec_envelope, reverse, reverb
 
