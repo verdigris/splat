b042eb6bbe6343270f70e7d08e392f4167a846a1
Guillaume Tucker <guillaume@mangoz.org>
2013-03-23 16:49:28 +0000

    test.py: use new Fragment keywords (duration)

--- test.py
+++ test.py
@@ -21,12 +21,12 @@ def check_md5(frag, hexdigest):
 # test functions
 
 def test_frag():
-    frag = splat.Fragment(2, 48000, 1.0)
+    frag = splat.Fragment(duration=1.0)
     return check_md5(frag, 'fe384f668da282694c29a84ebd33481d')
 test_frag.test_name = 'Fragment'
 
 def test_sine():
-    gen = splat.SineGenerator(splat.Fragment(2, 48000, 1.0))
+    gen = splat.SineGenerator(splat.Fragment(duration=1.0))
     gen.run(1000, 0.0, 1.0)
     return check_md5(gen.frag, 'ec18389e198ee868d61c9439343a3337')
 test_sine.test_name = "SineGenerator"
