e055d8e26dcf519c182d07665bb208e2a7232b2a
Guillaume Tucker <guillaume@mangoz.org>
2013-12-29 21:07:40 +0000

    _splat.c: add source helpers + tidy-up
    Add `splat_check_all_floats' to test whether a series of
    Python objects are floats using a variadic function.  This
    can be used to optimise the code when only floats are used,
    to avoid going through the signal vectors.
    
    Also add some comments to separate different sections in
    the file.

--- _splat.c
+++ _splat.c
@@ -20,14 +20,36 @@
 #include <Python.h>
 #include <math.h>
 
+/* Set to 1 to use 4xfloat vectors (SSE) */
+#define USE_V4SF 0
+
 #define BASE_TYPE_FLAGS (Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE)
 #define MAX_CHANNELS 16
 
 #define lin2dB(level) (20 * log10(level))
 #define dB2lin(dB) (pow10((dB) / 20))
 
-/* Set to 1 to use 4xfloat vectors (SSE) */
-#define USE_V4SF 1
+/* Use PP_RSEQ_N to get the number of arguments in __VA_ARGS__ */
+#define PP_NARG(...) \
+         PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
+#define PP_NARG_(...) \
+         PP_ARG_N(__VA_ARGS__)
+#define PP_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,N,...) N
+#define PP_RSEQ_N() \
+         63,62,61,60,                   \
+         59,58,57,56,55,54,53,52,51,50, \
+         49,48,47,46,45,44,43,42,41,40, \
+         39,38,37,36,35,34,33,32,31,30, \
+         29,28,27,26,25,24,23,22,21,20, \
+         19,18,17,16,15,14,13,12,11,10, \
+         9,8,7,6,5,4,3,2,1,0
 
 #if USE_V4SF
 typedef float v4sf __attribute__ ((vector_size(16)));
@@ -946,6 +968,36 @@ static PyObject *splat_dB2lin(PyObject *self, PyObject *args)
 	return PyFloat_FromDouble(dB2lin(dB));
 }
 
+/* ----------------------------------------------------------------------------
+ * Sources
+ */
+
+/* -- helpers for sources -- */
+
+#define splat_check_all_floats(...)			\
+	_splat_check_all_floats(PP_NARG(__VA_ARGS__), __VA_ARGS__)
+
+static int _splat_check_all_floats(size_t n, ...)
+{
+	va_list objs;
+	size_t i;
+
+	va_start(objs, n);
+
+	for (i = 0; i < n; ++i) {
+		PyObject *obj = va_arg(objs, PyObject *);
+
+		if (!PyFloat_Check(obj))
+			break;
+	}
+
+	va_end(objs);
+
+	return (i == n);
+}
+
+/* -- sine source -- */
+
 PyDoc_STRVAR(splat_sine_doc,
 "sine(fragment, frequency, levels)\n"
 "\n"
@@ -981,6 +1033,8 @@ static PyObject *splat_sine(PyObject *self, PyObject *args)
 
 	Py_RETURN_NONE;
 }
+
+/* -- square source -- */
 
 PyDoc_STRVAR(splat_square_doc,
 "square(fragment, frequency, levels, ratio=0.5)\n"
@@ -1035,6 +1089,8 @@ static PyObject *splat_square(PyObject *self, PyObject *args)
 
 	Py_RETURN_NONE;
 }
+
+/* -- triangle source -- */
 
 PyDoc_STRVAR(splat_triangle_doc,
 "triangle(fragment, frequency, levels, ratio=0.5)\n"
@@ -1098,6 +1154,8 @@ static PyObject *splat_triangle(PyObject *self, PyObject *args)
 
 	Py_RETURN_NONE;
 }
+
+/* -- overtones source -- */
 
 PyDoc_STRVAR(splat_overtones_doc,
 "overtones(fragment, frequency, levels, overtones)\n"
@@ -1199,6 +1257,10 @@ free_overtones:
 
 	Py_RETURN_NONE;
 }
+
+/* ----------------------------------------------------------------------------
+ * Filters
+ */
 
 PyDoc_STRVAR(splat_dec_envelope_doc,
 "dec_envelope(fragment, k=1.0, p=1.0)\n"
