d6a43ffb5913a12cdd269ab5fb0c2109d36a2dea
Guillaume Tucker <guillaume@mangoz.org>
2014-01-15 20:39:37 +0000

    setup.py, splat/__init__.py: v1.1 + add description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup, Extension
 
-setup(name='splat', version='0.0',
+setup(name='splat', version='1.1',
+      description="Splat - sound generator",
+      author="Guillaume Tucker - http://verdigris.mu",
+      author_email="guillaume@mangoz.org",
       ext_modules=[Extension('_splat', ['_splat.c'])],
       packages=['splat'])
--- splat/__init__.py
+++ splat/__init__.py
@@ -19,6 +19,8 @@ from _splat import lin2dB, dB2lin, sample_precision
 
 __all__ = ['gen', 'data', 'filters', 'sources', 'scales', 'interpol']
 
-VERSION = (1, 0)
+VERSION = (1, 1)
 VERSION_STR = '.'.join([str(v) for v in VERSION])
 BUILD = 6
+
+__version__ = VERSION_STR
