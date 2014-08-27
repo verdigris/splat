f06f455379308b67740e09b2bbbe6c4f56522266
Guillaume Tucker <guillaume@mangoz.org>
2014-08-27 23:42:23 +0100

    setup.py: rename splat package + improve
    This is to avoid a global package name clash with existing
    splat (SpeX Prism Library Analysis Toolkit).  The source code
    remains otherwise unchanged.
    
    * change name to 'verdigris.mu-splat'
    * add long_description
    * add Splat.pdf
    * add py_modules
    * add classifiers
    * add github.com URL

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
 from distutils.core import setup, Extension
 
-setup(name='splat', version='1.3',
+setup(name='verdigris.mu-splat', version='1.3',
       description="Splat - sound generator",
-      author="Guillaume Tucker - http://verdigris.mu",
+      author="Guillaume Tucker",
       author_email="guillaume@mangoz.org",
+      url="https://github.com/verdigris/splat",
+      py_modules=['test', 'example', 'dew_drop'],
       ext_modules=[Extension('_splat', ['_splat.c'])],
-      packages=['splat'])
+      packages=['splat'],
+      data_files=[('.', ['Splat.pdf',]),],
+      long_description="""
+The basic idea is to apply mathematical concepts to
+musical composition as well as sound synthesis. In practice,
+Splat lets you create just about any sound you can imagine
+and code in software.
+""",
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Editors',
+        'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        ])
