906091e7aa4fdcc58660a26cbcc0ff45c0e2ae6e
Guillaume Tucker <guillaume@mangoz.org>
2015-10-16 08:03:04 +0100

    add splat.tools.make to generate a makefile and run make
    This is to generate a makefile which can then be passed on to
    `make' to generate complex Splat pieces with several fragments in
    parallel which may depend on each other and on any extra static
    input files.
    
    The convention is that a top-level "mixer" module will generate
    the final sound file in a standard format such as WAV or Flac.
    It will be mixing together all the other fragments generated and
    saved in the Splat Audio Fragment .saf format.  Each python
    module is invoked in its own process and is being passed the
    output file name it needs to generate, so either a .saf for
    fragment modules and a standard audio format for the mixer
    module.  It's also possible to directly invoke each fragment
    module and make it generate its own sound fragment in an
    arbitrary format.
    
    * add splat.tools.make to import a top-level mixer module and
      find the dependencies on other sound fragment modules with
      SPLAT_FRAGS and any other file with SPLAT_DEPS then generate a
      makefile with it
    
    * also add splat.tools.make.main_mixer and .main_frag functions
      respectively to be used as standard main functions in top-level
      mixer and fragment modules
    
    * add make command to tools/splat to generate a makefile for a
      given top-level module on the fly and run `make' with it in one
      command with make options passed with --opt, for example:
    
        splat make my_mixer --opt="-j8"
    
    * add --print option to the splat `make' command to only print
      the generated make file and not run `make', for example to save
      it to a file:
    
        splat make my_mixer --print > Makefile

---
+++ splat/tools/make.py
@@ -0,0 +1,143 @@
+# Splat - splat/tools/make.py
+#
+# Copyright (C) 2015, 2016 Guillaume Tucker <guillaume@mangoz.org>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+import sys
+import subprocess
+import shlex
+import argparse
+import splat
+import splat.data
+import splat.filters
+
+def build_dep_tree(mod, name, dep_tree, clean_list):
+    # Generic files this module directly depends on
+    deps = getattr(mod, 'SPLAT_DEPS', list())
+
+    # Splat fragment modules this module depends on
+    frags = getattr(mod, 'SPLAT_FRAGS', None)
+    if frags is not None:
+        if isinstance(frags, dict):
+            frag_seq = frags.iterkeys()
+        else:
+            frag_seq = frags
+        for frag in frag_seq:
+            target = '.'.join([frag, 'saf'])
+            deps.append(target)
+            clean_list.append(target)
+            if target not in dep_tree:
+                frag_mod = __import__(frag)
+                build_dep_tree(frag_mod, target, dep_tree, clean_list)
+
+    # Add this module to the tree with all the things it depends on
+    dep_tree[name] = deps
+
+def makefile(main, fmt):
+    mk = """\
+ver_check := $(shell python -c 'import splat; splat.check_version(({}, {}))' \\
+	|| echo ERROR)
+
+ifeq ($(ver_check),ERROR)
+  $(error 'Unmet required Splat version')
+endif
+
+""".format(*splat.VERSION)
+
+    mk += """\
+%.saf: %.py
+	@echo "  GEN     " $@
+	@python $< $@
+
+%.{}: %.py
+	@echo "  MIX     " $@
+	@python $< $@
+""".format(fmt)
+
+    main_mix = "{}.{}".format(main, fmt)
+    dep_tree = dict()
+    clean_list = [main_mix]
+    main_mod = __import__(main)
+    build_dep_tree(main_mod, main_mix, dep_tree, clean_list)
+    mk += '\nall: {}\n\n'.format(main_mix)
+    for target, deps in dep_tree.iteritems():
+        if deps:
+            mk += target + ':'
+            for dep in deps:
+                mk += " \\\n\t{}".format(dep)
+            mk += '\n\n'
+
+    mk += """\
+
+.PHONY: clean
+
+clean:
+"""
+    for target in clean_list:
+        mk += "\t@echo \"  CLEAN    {}\"\n".format(target)
+        mk += "\t@rm -f {}\n".format(target)
+
+    return mk
+
+def run_make(mk, opts=None):
+    args = ['make', '-f', '-']
+    if opts:
+        args += opts
+    p = subprocess.Popen(args, stdin=subprocess.PIPE)
+    p.communicate(input=mk)
+    return p.returncode
+
+def main(argv):
+    parser = argparse.ArgumentParser("Generate Splat makefile and run make")
+    parser.add_argument('main',
+                        help="name of the mixer module")
+    parser.add_argument('--format', '-f', default="wav",
+                        help="output audio format")
+    parser.add_argument('--print', action='store_true', default=False,
+                        help="print makefile and do not run make")
+    parser.add_argument('--opt', '-o', help="options to pass to make")
+    args = parser.parse_args(argv[1:])
+
+    mk = makefile(args.main, args.format)
+    if args.print:
+        print(mk)
+        return True
+    else:
+        opt = shlex.split(args.opt) if args.opt else None
+        ret = run_make(mk, opt)
+        return True if (ret == 0) else False
+
+if __name__ == '__main__':
+    ret = main(sys.argv)
+    sys.exit(0 if ret is True else 1)
+
+# standard main function for a mixer target
+def main_mixer(argv, fragments, master=None, filters=None):
+    f_out = argv[1]
+    if master is None:
+        master = splat.data.Fragment()
+    for frag, (ofst, ml) in fragments.iteritems():
+        fname = '.'.join([frag, 'saf'])
+        master.mix(splat.data.Fragment.open(fname), offset=ofst, levels=ml)
+    if filters is not None:
+        splat.filters.FilterChain(filters).run(master)
+    master.save(f_out)
+
+# standard main function for a fragment target
+def main_frag(argv, func):
+    f_out = argv[1]
+    frag = func(argv[1:])
+    frag.save(f_out)
