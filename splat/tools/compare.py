f6ab5d88a28581c10edc7021c234c8b84cfaec71
Guillaume Tucker <guillaume@mangoz.org>
2016-06-19 09:47:20 +0000

    splat.tools.compare: add docstrings
    Add module docstring with full documentation for the ``splat
    compare`` command.

--- splat/tools/compare.py
+++ splat/tools/compare.py
@@ -15,12 +15,58 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Summary
+^^^^^^^
+
+The ``splat compare`` command is a simple utility to compare two sound
+files.  It will save the difference created by substracting
+corresponding samples from one file to the other and print the peak
+amplitude of the difference in dB.
+
+Syntax
+^^^^^^
+
+This is the output of ``splat compare --help``::
+
+  usage: Compare sound files and show the peak signal difference
+         [-h] [--save SAVE] f1 f2
+
+  positional arguments:
+    f1           path to first sound file
+    f2           path to second sound file
+
+  optional arguments:
+    -h, --help   show this help message and exit
+    --save SAVE  path to save the difference
+
+For example, to compare two files, print the peak difference in dB and
+save the result of the substracted fragments in ``delta.wav``::
+
+  $ splat compare file-1.wav file-2.wav --save=delta.wav
+  Peak delta: -45.803 dB
+
+Functions
+^^^^^^^^^
+
+The following function can also be imported to perform the same
+comparision between Fragment objects directly in Python instead of via
+the command line:
+
+.. autofunction:: splat.tools.compare.frag_delta
+"""
+
 import sys
 import argparse
 import splat
 import splat.data
 
 def frag_delta(f1, f2):
+    """Return a fragment with the difference between two fragments.
+
+    Fragment ``f2`` gets substracted from ``f1`` and a new fragment
+    with the difference is returned.  The input fragments are not
+    modified by this operation.
+    """
     delta = f2.dup()
     delta.amp(-1)
     delta.mix(f1)
