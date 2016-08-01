26c17ca63ce761ae6eb243f2ec13bbbb06b32f88
Guillaume Tucker <guillaume@mangoz.org>
2016-08-01 21:59:13 +0100

    build-0023

--- splat/__init__.py
+++ splat/__init__.py
@@ -25,7 +25,7 @@ __all__ = ['gen', 'data', 'filters', 'sources', 'scales', 'interpol', 'seq']
 
 VERSION = (1, 6)
 VERSION_STR = '.'.join([str(v) for v in VERSION])
-BUILD = 22
+BUILD = 23
 
 __version__ = VERSION_STR
 
