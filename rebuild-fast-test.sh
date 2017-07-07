f39a1ebe6c11309fab08d1eab094c1c642191e08
Guillaume Tucker <guillaume@mangoz.org>
2017-07-07 07:33:55 +0000

    rebuild-fast-test.sh: guess SIMD variant based on CPU arch
    Use `uname -m' to get the name of the CPU architecture and pick the
    corresponding SIMD variant if it's a known architecture.

--- rebuild-fast-test.sh
+++ rebuild-fast-test.sh
@@ -2,7 +2,27 @@
 
 set -e
 
-simd="${1-sse}"
+simd="$1"
+
+if [ -z "$simd" ]; then
+    arch=$(uname -m)
+
+    case "$arch" in
+        "armv7l")
+            simd=neon-v7
+            ;;
+	"aarch64")
+	    simd=neon-v8
+	    ;;
+        "x86_64")
+            simd=sse
+            ;;
+        *)
+            echo "Unknown architecture, please specify"
+            ;;
+    esac
+fi
+
 [ -x build-"$simd" ] || {
     echo "Unsupported SIMD mode: $simd"
     exit 1
