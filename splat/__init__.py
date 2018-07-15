627c66893c65f2b9c3d1fa363ceb2fc8d1f3800f
Guillaume Tucker <guillaume.tucker@gmail.com>
2018-07-15 14:38:08 +0100

    build-0026

--- splat/__init__.py
+++ splat/__init__.py
@@ -26,7 +26,7 @@ __all__ = ['gen', 'data', 'filters', 'sources', 'scales', 'interpol', 'seq']
 
 VERSION = (1, 6)
 VERSION_STR = '.'.join([str(v) for v in VERSION])
-BUILD = 25
+BUILD = 26
 
 __version__ = VERSION_STR
 
